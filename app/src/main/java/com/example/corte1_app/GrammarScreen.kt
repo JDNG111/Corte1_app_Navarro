c9959c10c0493d61f21e78415dcf9d986c0ec8a5
JDNG111 <115641045+JDNG111@users.noreply.github.com>
2025-03-29 17:34:24 -0500

    Update GrammarScreen.kt

--- app/src/main/java/com/example/corte1_app/GrammarScreen.kt
+++ app/src/main/java/com/example/corte1_app/GrammarScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.ui.*
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+//Pantalla de ejercicio de ingles
+
 @Composable
 fun GrammarScreen(navController: NavController) {
     val exercises = listOf(
